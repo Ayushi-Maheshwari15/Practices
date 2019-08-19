fadfbd5700dee746a095b8a8b8f095384b78ef8d
Ayushi-Maheshwari15 <123ayushimaheshwari@gmail.com>
2019-08-20 00:11:50 +0530

    take the input by integer

--- src/Sample.java
+++ src/Sample.java
@@ -1,2 +1,13 @@
+import java.util.*;
 public class Sample {
+    public static void main(String[] args) {
+        int n;
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Enter the number");
+        n=scan.nextInt();
+        for (int i = 0; i <n ; i++) {
+            System.out.println(i);
+
+        }
+    }
 }
